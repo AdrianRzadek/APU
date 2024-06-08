39e93c6678f6543690e59ef671a9fa15988d72ea
AdrianRzadek <mschxd@gmail.com>
2024-06-08 20:17:21 +0200

    Update 4_1.MLR.R

--- 4.MLR/4_1.MLR.R
+++ 4.MLR/4_1.MLR.R
@@ -1,18 +1,60 @@
 #install.packages("party")
 #install.packages("mlr")
+#install.packages("C50")
+#install.packages("mlr3learners")
 library(readr)
 library(dplyr)
 library(party)
 library(rpart)
-library(mlr)
+library(mlr3)
+#library(mlr)
 library(MASS)
+library(C50)
+library(mlr3learners)
 
 data("Melanoma")
 
-melanoma_task <- makeClassifTask(data = Melanoma, target = "sex")
 
-melanoma_tree <- rpart(status ~ ., data = Melanoma, method = "class")
-
-par(mar = c(1, 1, 1, 1))
+#melanoma_task <- makeClassifTask(status ~ ., data = Melanoma, target = "sex")
+melanoma_tree <- rpart(sex ~ ., data = Melanoma, method = "class")
+summary(melanoma_tree)
+tree_fit <- ctree(status ~ ., data = Melanoma)
 plot(melanoma_tree)
 text(melanoma_tree, use.n = TRUE)
+plot(tree_fit)
+
+learners <- mlr_learners
+print(learners)
+
+
+task = as_task_classif(sex ~ ., data = Melanoma)
+task
+learner = lrn("classif.rpart", cp = .01)
+
+
+split = partition(task, ratio = 0.67)
+
+# train the model
+learner$train(task, split$train)
+
+# predict data
+prediction = learner$predict(task, split$test_set)
+
+# calculate performance
+prediction$confusion
+
+
+measure = msr("classif.acc")
+prediction$score(measure)
+ 
+
+ # 3-fold cross validation
+resampling = rsmp("cv", folds = 3L)
+
+# run experiments
+rr = resample(task, learner, resampling)
+
+# access results
+rr$score(measure)[, .(task_id, learner_id, iteration, classif.acc)]
+
+rr$aggregate(measure)
